8c0595e5ca764b82d66639230334e3fd7a1c0537
boxstream <taylor.monacelli@streambox.com>
2010-01-28 00:39:45 -0800

    Updates

--- emacs_install.bat
+++ emacs_install.bat
@@ -3,6 +3,23 @@ REM Last modified $Id$
 REM $HeadURL$
 REM -*- bat -*-
 
+echo %cd%
 unzip -q -n emacs-23.1-bin-i386.zip
 move emacs-23.1 "%programfiles%"
 
+:: ------------------------------
+:: Fix known bug on windows:
+:: http://stackoverflow.com/questions/885793/emacs-error-when-calling-server-start
+:: ------------------------------
+
+REM Warning (initialization): An error occurred while loading `c:/cygwin/home/administrator/.emacs':
+REM error: The directory c:/cygwin/home/administrator/.emacs.d/server is unsafe
+
+pushd "%programfiles%\emacs-23.1\lisp"
+echo %cd%
+move server.elc server.elc.orig
+move server.el server.el.orig
+popd
+
+echo %cd%
+copy /y "deploy\Program Files\emacs_windows_fix\server.el" "%programfiles%\emacs-23.1\lisp"
