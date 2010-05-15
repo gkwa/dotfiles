2aa6f0e2cb7e57ff3f7bb0daf7aac67e1f34b2ea
boxstream <taylor.monacelli@streambox.com>
2010-05-14 20:51:39 -0700

    Flipped crlf on setup-overwrite.bat

--- cygwin_setup/setup-overwrite.bat
+++ cygwin_setup/setup-overwrite.bat
@@ -1,1 +1,11 @@
-@Echo offREM Last modified $Id$REM $HeadURL$REM -*- bat -*-move "deploy\Program Files\CygwinInstall\setup.exe" "Program Files\CygwinInstall\setup.exe.new"cd %systemdrive%del /q "Program Files\CygwinInstall\setup.exe.old"move "Program Files\CygwinInstall\setup.exe" "Program Files\CygwinInstall\setup.exe.old"move "Program Files\CygwinInstall\setup.exe.new" "Program Files\CygwinInstall\setup.exe"
+@Echo off
+REM Last modified $Id$
+REM $HeadURL$
+REM -*- bat -*-
+
+
+move "deploy\Program Files\CygwinInstall\setup.exe" "Program Files\CygwinInstall\setup.exe.new"
+cd %systemdrive%
+del /q "Program Files\CygwinInstall\setup.exe.old"
+move "Program Files\CygwinInstall\setup.exe" "Program Files\CygwinInstall\setup.exe.old"
+move "Program Files\CygwinInstall\setup.exe.new" "Program Files\CygwinInstall\setup.exe"
