aeb50ca0d4e4c87037501b2784bf2102c90d2b8c
boxstream <taylor.monacelli@streambox.com>
2010-06-28 09:13:15 -0700

    Fix csetup to switch to the %systemdrive% before trying to create c.bat

--- cygwin_setup/csetup.bat
+++ cygwin_setup/csetup.bat
@@ -7,6 +7,8 @@ REM -*- bat -*-
 :: put c.bat in %systemroot%\system32 to enable quick cygwin start
 :: convenience method
 :: ------------------------------
+:: yes you need this first to switch to the correct drive
+%systemdrive%
 cd "%systemroot%\system32"
 echo @Echo off					> c.bat
 echo cd %systmdrive%\cygwin			>> c.bat
