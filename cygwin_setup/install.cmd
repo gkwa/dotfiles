bfeda52044fb2a817a518c30c090f175226cd146
boxstream <taylor.monacelli@streambox.com>
2010-09-01 22:43:40 -0700

    Upgraded cygwin to fix mount.exe problem.  It didn't support --show-cygdrive-prefix properly

--- cygwin_setup/install.cmd
+++ cygwin_setup/install.cmd
@@ -3,17 +3,6 @@ REM Last modified $Id$
 REM $HeadURL$
 REM -*- bat -*-
 
-:: purpose:
-:: 
-:: download robocopy
-:: copy uninstalled cygwin stuff to local drive
-:: install cygwin stuff to local drive
-::  a. setup.exe relies on setup.ini, not installed.db
-::
-
-:: if exist %systemdrive%\cygwin\nul goto done
-
-mkdir %systemdrive%\cygwin\etc\setup
 cd cygwin_setup
 
 :: deploy\cygwin and deploy\Program Files will get pushed to %systemdrive%\cygwin %systemdrive%\Program Files
@@ -24,7 +13,7 @@ cd "%programfiles%\CygwinInstall"
 
 set target=%systemdrive%
 :: somehow, miraculously, this finds the directory "http%3a%2f%2fcygwin.lilengine.com%2f"
-# CLI arguments documented here: http://www.cygwin.com/faq/faq.setup.html#faq.setup.cli
+:: CLI arguments documented here: http://www.cygwin.com/faq/faq.setup.html#faq.setup.cli
 .\setup --no-desktop --quiet-mode --local-install --local-package-dir "%ProgramFiles%\CygwinInstall" 
 
 setx CYGWIN "binmode ntsec tty" -m 
@@ -35,42 +24,6 @@ setx CYGWIN "binmode ntsec tty" -m
 goto end
 
 :end
-
-
-
-
-
-
-:: Old stuff below
-
-:: :: ------------------------------
-:: :: copy cygwin stuff to local drive
-:: :: ------------------------------
-:: set src=\\10.0.2.10\Development\Cygwin\deploy\cygwin
-:: set target=%systemdrive%
-:: robocopy "%src%" %systemdrive%\cygwin /np /e /fp /tee /log:%windir%\temp\cygwin_setup.log
-
-:: set src=\\10.0.2.10\Development\Cygwin\deploy\Program Files
-:: set target=%systemdrive%\Program Files
-:: robocopy "%src%" "%target%" /np /e /fp /tee /log:%windir%\temp\cygwin_setup.log
-
-:: :: ------------------------------
-:: :: instal cygwin stuff
-:: :: ------------------------------
-:: pushd "%target%\CygwinInstall"
-:: .\setup --no-desktop --quiet-mode --local-install --local-package-dir "%target%\CygwinInstall" 
-:: :: somehow, miraculously, this finds the directory "http%3a%2f%2fcygwin.lilengine.com%2f"
-:: popd
-
-:: :done
-
-
-:: :: echo c:\image_backup.bat reporting | blat - -subject "RD Ghost backup log" -attacht %log% -server 10.0.2.69 ^
-:: ::   -f bugzilla@streambox.com -sender RD_Ghost@streambox.com -to root@[10.0.2.69]
-
-:: :: echo c:\image_backup.bat reporting | blat - -bodyF c:\image_backup_mail.txt -subject "RD Ghost backup log" -attacht %log% -server 10.0.2.69 ^
-:: ::   -f bugzilla@streambox.com -sender RD_Ghost@streambox.com -to taylor.monacelli@streambox.com,tho@streambox.com
-:: ping -n 20 127.0.0.1
 
 :: Emacs vars
 :: Local Variables: ***
