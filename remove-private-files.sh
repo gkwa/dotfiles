3bfade3333f1c53bb87630b8760a8480e036b504
taylormonacelli <tailor@u.washington.edu>
2014-06-08 11:02:27 -0700

    Use the BFG tool to remove files since it seems fast

--- remove-private-files.sh
+++ remove-private-files.sh
@@ -1,6 +1,37 @@
 #/bin/sh
 
-git filter-branch \
-    --force \
-    --index-filter 'git rm --cached --ignore-unmatch notes.txt' \
-    --prune-empty --tag-name-filter cat -- --all
+# git filter-branch \
+#     --force \
+#     --index-filter 'git rm --cached --ignore-unmatch notes.txt' \
+#     --prune-empty --tag-name-filter cat -- --all
+
+set -o errexit
+
+bfgjar=/Users/demo/Downloads/bfg-1.11.6.jar
+
+cd /tmp
+rm -rf /tmp/demo.git
+rm -rf /tmp/demo
+
+if test -d /tmp/demo.git
+then
+    echo didn\'t remove /tmp/demo.git, quitting.
+    exit 1
+fi
+
+if test -d /tmp/demo
+then
+    echo didn\'t remove /tmp/demo.git, quitting.
+    exit 1
+fi
+
+git clone --mirror /Users/demo /tmp/demo.git
+du -sh /tmp/demo.git
+# java -jar $bfgjar --delete-files notes.txt /tmp/demo.git
+java -jar $bfgjar --delete-files kee.kbd /tmp/demo.git
+cd /tmp/demo.git
+git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+du -sh /tmp/demo.git
+git clone /tmp/demo.git /tmp/demo
+cd /tmp/demo
