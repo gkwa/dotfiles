1819de47fd6d104c1aee95aced02d4e4f306b78e
taylormonacelli <tailor@u.washington.edu>
2014-06-08 13:38:50 -0700

    yada-yada

--- remove-private-files.sh
+++ remove-private-files.sh
@@ -27,7 +27,7 @@ fi
 
 git clone --mirror --reference /Users/demo dev:~/proj/emacs.git /tmp/dotfiles.git
 du -sh /tmp/dotfiles.git
-java -jar $bfgjar --delete-files '{kee.kdb,notes.txt,.alias,unzip.exe}' --delete-folders '{emacs,notes}' /tmp/dotfiles.git
+java -jar $bfgjar --strip-blobs-bigger-than 1M --delete-files '{kee.kdb,notes.txt,.alias,unzip.exe}' --delete-folders '{emacs,notes}' /tmp/dotfiles.git
 cd /tmp/dotfiles.git
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
