ae1e3d2394a9e73b64f29cd122ad50550b0d72f4
taylormonacelli <tailor@u.washington.edu>
2014-06-08 12:23:30 -0700

    yada-yada

--- remove-private-files.sh
+++ remove-private-files.sh
@@ -26,7 +26,7 @@ fi
 
 git clone --reference /Users/demo --mirror dev:~/proj/emacs.git /tmp/emacs.git
 du -sh /tmp/emacs.git
-java -jar $bfgjar --delete-files '{notes.txt,kee.kbd}' --delete-folders 'notes' /tmp/emacs.git
+java -jar $bfgjar --delete-files '{notes.txt,kee.kbd}' --delete-folders '{notes,emacs}' /tmp/emacs.git
 cd /tmp/emacs.git
 git reflog expire --expire=now --all
 git gc --prune=now --aggressive
