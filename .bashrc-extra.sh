2157e9b989cf7bc25998011a3391e02000b0ca15
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-08-14 19:51:24 -0700

    .bashrc-extra.sh: add chef cookbook() function to create new cookbook

--- .bashrc-extra.sh
+++ .bashrc-extra.sh
@@ -130,3 +130,11 @@ function osxbackup(){
 			/Users /Volumes/Backup
 	fi
 }
+
+function cookbook(){ 
+	cookbook=$1
+	chef generate cookbook --email taylormonacelli@gmail.com $cookbook
+	rm -rf $cookbook/.delivery/build_cookbook $cookbook/.delivery/config.json
+	echo 'remote_file = "https://raw.githubusercontent.com/chef-cookbooks/community_cookbook_tools/master/delivery/project.toml"' \
+		 >$cookbook/.delivery/project.toml
+}
