959d3de75dce89f3e6e4c7b4d6ff4668d869f887
Taylor Monacelli <taylor.monacelli@servicenow.com>
2019-01-06 20:03:09 -0800

    Add .hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,32 @@
+-- --------------------------------------------------
+
+local hyper = {"ctrl", "alt", "cmd"}
+
+hs.loadSpoon("MiroWindowsManager")
+
+hs.window.animationDuration = 0.2
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"}
+})
+
+-- --------------------------------------------------
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
+-- --------------------------------------------------
