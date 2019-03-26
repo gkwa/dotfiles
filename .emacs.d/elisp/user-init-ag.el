34def057e1cadb374362e5c925f3e87d8df7e459
Taylor Monacelli <taylormonacelli@gmail.com>
2019-03-25 17:42:33 -0700

    emacs-helm

--- .emacs.d/elisp/user-init-ag.el
+++ .emacs.d/elisp/user-init-ag.el
@@ -1,3 +1,9 @@
 (ensure-package-installed 'ag)
 
+(custom-set-variables
+ '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
+ '(helm-ag-command-option "--all-text")
+ '(helm-ag-insert-at-point 'symbol)
+ '(helm-ag-ignore-buffer-patterns '("\\.txt\\'" "\\.mkd\\'")))
+
 (provide 'user-init-ag)
