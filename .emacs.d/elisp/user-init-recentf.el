f1349c1bae1ca3ea6e2a9357f502fdbc24a09b3c
mtmonacelli <taylormonacelli@gmail.com>
2020-03-11 21:45:53 -0700

    emacs: improve helm recent file search

--- .emacs.d/elisp/user-init-recentf.el
+++ .emacs.d/elisp/user-init-recentf.el
@@ -1,7 +1,7 @@
 (autoload 'recentf "RecentF" "Enter recentf mode." t)
-(setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
-(setq recentf-max-saved-items 100)
-(setq recentf-max-menu-items 100)
+(setq recentf-auto-cleanup 3600)
+(setq recentf-max-saved-items 50)
+(setq recentf-max-menu-items 50)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 (provide 'user-init-recentf)
