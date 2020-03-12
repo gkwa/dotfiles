b5144bb743bb650d890ad2cb3969ce8429ffb453
mtmonacelli <taylormonacelli@gmail.com>
2020-03-12 09:52:15 -0700

    emacs: remove noise

--- .emacs.d/elisp/user-init-recentf.el
+++ .emacs.d/elisp/user-init-recentf.el
@@ -1,9 +1,7 @@
-;; http://www.emacswiki.org/emacs/RecentFiles
 (autoload 'recentf "RecentF" "Enter recentf mode." t)
 (setq recentf-auto-cleanup 'never) ;; disable before we start recentf!
 (setq recentf-max-saved-items 100)
 (setq recentf-max-menu-items 100)
-;;(recentf-mode 1)
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
 
 (provide 'user-init-recentf)
