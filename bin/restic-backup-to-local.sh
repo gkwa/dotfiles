ee0187fc16373022e96db62c52a253597405c585
mtmonacelli <taylormonacelli@gmail.com>
2020-04-13 05:29:58 -0700

    restic: backup config

--- bin/restic-backup-to-local.sh
+++ bin/restic-backup-to-local.sh
@@ -6,4 +6,4 @@ source ~/bin/restic-backup-variables.sh
 
 export RESTIC_REPOSITORY=/Volumes/backup/restic
 
-time restic backup /Users --exclude-file=$RESTIC_BACKUP_EXCLUDE_FROM_FILE_PATH
+time restic backup /Users --cleanup-cache --exclude-file=$RESTIC_BACKUP_EXCLUDE_FROM_FILE_PATH
--- bin/restic-backup-to-remote.sh
+++ bin/restic-backup-to-remote.sh
@@ -6,6 +6,6 @@ source ~/bin/restic-backup-variables.sh
 
 export RESTIC_REPOSITORY="b2:macos-laptop"
 
-time restic backup /Users --exclude-file=$RESTIC_BACKUP_EXCLUDE_FROM_FILE_PATH
+time restic backup /Users --cleanup-cache --exclude-file=$RESTIC_BACKUP_EXCLUDE_FROM_FILE_PATH
 
 
