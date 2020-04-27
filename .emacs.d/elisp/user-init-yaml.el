804389a4e4dfbc737cb63c8257cea608be28929b
Taylor Monacelli <taylormonacelli@gmail.com>
2020-04-27 05:18:12 -0700

    emacs: yml file opens in yaml mode

--- .emacs.d/elisp/user-init-yaml.el
+++ .emacs.d/elisp/user-init-yaml.el
@@ -2,4 +2,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.eyaml\\'" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode))
+(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (provide 'user-init-yaml)
