79c1f203e372078ff7d245e1ac2b1fbbf55d8e3c
Taylor Monacelli <taylormonacelli@gmail.com>
2020-07-05 12:45:16 -0700

    Add function to cleanup python scripts

--- .bash.d/python3.sh
+++ .bash.d/python3.sh
@@ -1,1 +1,12 @@
 [[ -d ~/.virtualenvs/.py38 ]] && source ~/.virtualenvs/.py38/bin/activate
+
+cuppy() {
+    path="$*"
+    for file in $path; do
+        autoflake --in-place --remove-all-unused-imports "$file"
+        black "$file"
+        isort "$file"
+    done
+}
+
+export -f cuppy
