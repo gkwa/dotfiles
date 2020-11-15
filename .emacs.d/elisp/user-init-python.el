b2e596dec7bd31f69e2f767629fe91ff46388dac
Taylor Monacelli <taylormonacelli@gmail.com>
2020-11-14 20:24:57 -0800

    emacs: python indent 4

--- .emacs.d/elisp/user-init-python.el
+++ .emacs.d/elisp/user-init-python.el
@@ -3,6 +3,6 @@
 (ensure-package-installed 'blacken)
 
 (add-hook 'python-mode-hook 'blacken-mode)
-(setq-default python-indent-offset 2)
+(setq-default python-indent-offset 4)
 
 (provide 'user-init-python)
