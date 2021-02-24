c71413c927ec2e576f00932dfec960ffa5a35d06
Taylor Monacelli <taylormonacelli@gmail.com>
2021-02-24 10:17:49 -0800

    Show python's venv in bash prompt

--- .bash.d/direnv.sh
+++ .bash.d/direnv.sh
@@ -3,3 +3,13 @@
 if command direnv 2>&1 >/dev/null; then
     eval "$(direnv hook bash)"
 fi
+
+show_virtual_env() {
+  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+    echo "($(basename $VIRTUAL_ENV))"
+  fi
+}
+
+export -f show_virtual_env
+
+PS1='$(show_virtual_env)'$PS1
