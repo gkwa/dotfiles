7844830e8ab436d3e00794abbf1adbfaa28c6b06
Taylor Monacelli <taylormonacelli@gmail.com>
2021-03-25 06:37:00 -0700

    Add keybindings for fzf

--- .bash.d/fzf.sh
+++ .bash.d/fzf.sh
@@ -1,1 +1,15 @@
-[[ -f ~/.fzf.bash ]] && . ~/.fzf.bash
+# https://github.com/junegunn/fzf
+
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+[ -s /usr/local/opt/fzf/shell/key-bindings.bash ] && source /usr/local/opt/fzf/shell/key-bindings.bash
