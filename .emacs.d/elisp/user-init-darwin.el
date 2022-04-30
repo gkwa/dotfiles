58e7210c0db9e3176113498baf0e764c477c1805
Your Name <you@example.com>
2022-04-30 12:35:14 -0700

    emacs: refactor pbcopy

--- .emacs.d/elisp/user-init-darwin.el
+++ .emacs.d/elisp/user-init-darwin.el
@@ -1,8 +1,5 @@
 (when (equal system-type 'darwin)
   (progn
-    (require 'user-init-pbcopy)
-    (require 'pbcopy)
-    (turn-on-pbcopy)
     ;; On osx, when I try C-DEL, then all I get is DEL. I want to bind
     ;; C-DEL to backward-kill-word, but since I can't generate the
     ;; C-DEL key, I'm using the function key fn
--- .emacs.d/elisp/user-init-pbcopy.el
+++ .emacs.d/elisp/user-init-pbcopy.el
@@ -1,4 +1,8 @@
-(if (eq system-type 'darwin)
-  (turn-on-pbcopy)
-  )
+(provide system-type 'darwin)
+  (progn
+    (require 'user-init-pbcopy)
+    (require 'pbcopy)
+    (turn-on-pbcopy)
+    ))
+
 (provide 'user-init-pbcopy)
