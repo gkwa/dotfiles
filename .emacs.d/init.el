b3f3fa3d2b20bd86a7d47d72baef344a8b1566a6
Your Name <you@example.com>
2022-04-30 13:09:06 -0700

    emacs: suppress Warning (package): Unnecessary call to ‘package-initialize’ in init file Disable showing Disable logging

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
+(setq warning-suppress-log-types '((package reinitialization)))
+
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (add-to-list 'load-path "/Users/mtm/.emacs.d/.cask/28.1/elpa")
-
 (require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
 (cask-initialize)
 (require 'user-init)
