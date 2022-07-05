76e72cdfd2ffea834ffbea4e15fa9e051e1e455e
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-05 08:48:37 -0700

    Remove unused genpasswd fcn

--- .bash.d/common.sh
+++ .bash.d/common.sh
@@ -11,12 +11,5 @@ function notec() {
     git commit -m "Cleanup whitespace"
 }
 
-genpasswd() {
-    # from here: http://www.cyberciti.biz/tips/linux-unix-bsd-openssh-server-best-practices.html
-    local l=$1
-    [ "$l" == "" ] && l=20
-    tr -dc A-Za-z0-9_ </dev/urandom | head -c ${l} | xargs
-}
-
 [[ -d $HOME/bin ]] && export PATH=$HOME/bin:$PATH
 [[ -d $HOME/bin/streambox ]] && export PATH=$HOME/bin/streambox:$PATH
