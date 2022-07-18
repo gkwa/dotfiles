83115e84288157c8822bb2c96099cb1e89570e4b
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-18 13:01:04 -0700

    bash: nvm: pretty docs

--- .bash.d/nvm.sh
+++ .bash.d/nvm.sh
@@ -1,2 +1,21 @@
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+if [[ $OSTYPE == 'darwin'* ]]; then
+
+  # brew install nvm
+
+  export NVM_DIR="$HOME/.nvm"
+
+  # load nvm:
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"
+
+  # load nvm bash_completion:
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+
+else
+  export NVM_DIR="$HOME/.nvm"
+
+  # load nvm:
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  # load nvm bash_completion:
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+fi
