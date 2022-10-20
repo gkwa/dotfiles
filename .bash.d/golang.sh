63f3afa9b7ecf8701647fb0601952313ba94d1c4
Taylor Monacelli <taylormonacelli@gmail.com>
2022-10-20 12:52:29 -0700

    bash: add ~/go/bin to $PATH

--- .bash.d/golang.sh
+++ .bash.d/golang.sh
@@ -1,5 +1,6 @@
 [[ -d /usr/local/opt/go/libexec ]] && GOROOT=/usr/local/opt/go/libexec
 [[ -d $HOME/go ]] && GOPATH=$HOME/go
+[[ -d $HOME/go/bin ]] && GOPATH=$HOME/go/bin
 
 PATH=$PATH:$GOPATH/bin
 PATH=$PATH:$GOROOT/bin
