f18244c388c00d74f78e331eb420b3f939f6ea3b
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-30 12:59:57 -0800

    silly stuff

--- .bash.d/bash-darwin.sh
+++ .bash.d/bash-darwin.sh
@@ -2,6 +2,10 @@ if [[ $OSTYPE =~ ^darwin ]]; then
     book() {
         mdfind -onlyin ~/Documents/books -name "$*"
     }
+
+    # https://apple.stackexchange.com/a/371998
+    export BASH_SILENCE_DEPRECATION_WARNING=1
+
     . ~/bin/keychain
 fi
 
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,7 +19,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(go-mode bind-key use-package ledger-mode helm)))
+ '(package-selected-packages '(bind-key use-package ledger-mode helm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
