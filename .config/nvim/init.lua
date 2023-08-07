f11ca20d84ac6fa698ff88b0429f08ccc21c34f4
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-07 09:33:14 -0700

    Add neovim init

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.expandtab = false
+vim.opt.hlsearch = false
+vim.opt.autoindent = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = false
+vim.opt.number = true
+vim.opt.smartcase = true
+vim.opt.tabstop = 4
+vim.opt.wrap = false
