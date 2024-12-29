1da2b4b105073a7cde99625861ebebca39725181
Taylor Monacelli <taylormonacelli@gmail.com>
2024-12-28 21:18:51 -0500

    Update .bash.d/atuin.sh

--- dot_bash.d/atuin.sh
+++ dot_bash.d/atuin.sh
@@ -1,4 +1,6 @@
 command -v atuin 2>/dev/null || return
 
 eval "$(atuin init bash --disable-up-arrow --disable-ctrl-r)"
+
+# https://docs.atuin.sh/configuration/key-binding/#bash
 bind -x '"\C-]": "__atuin_history"'
