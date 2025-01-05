b686f1c6b452249fff65538431964e122a2048f6
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-05 10:02:34 -0500

    Update .bash.d/atuin.sh

--- dot_bash.d/atuin.sh
+++ dot_bash.d/atuin.sh
@@ -2,6 +2,9 @@
 
 ! command -v atuin &>/dev/null && return
 
+[[ -f $HOME/.atuin/bin/env ]] && . "$HOME/.atuin/bin/env"
+[[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
+
 eval "$(atuin init bash --disable-up-arrow --disable-ctrl-r)"
 
 # https://docs.atuin.sh/configuration/key-binding/#bash
