a35f8e73a70857c95f827f5c6faa859563536602
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-09 09:55:20 -0800

    Update .bash.d/git.sh

--- dot_bash.d/executable_git.sh
+++ dot_bash.d/executable_git.sh
@@ -2,11 +2,10 @@
 
 ! command -v git &>/dev/null && return
 
+export GIT_PAGER=cat
+
 function parse_git_branch() {
     ref=$(git symbolic-ref HEAD 2>/dev/null) || return
-    echo "("")${ref#refs/heads/}"")"
+    echo "(""${ref#refs/heads/}"")"
 }
-
 PS1="[\u@\h:\W\$(parse_git_branch)]$ "
-
-export GIT_PAGER=cat
