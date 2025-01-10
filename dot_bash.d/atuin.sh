f1f44983c972c512df2cc40d050e6455c51b1321
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-10 08:30:36 -0800

    Update .bash.d/atuin.sh

--- dot_bash.d/atuin.sh
+++ dot_bash.d/atuin.sh
@@ -4,9 +4,13 @@
 
 . "$HOME/.atuin/bin/env"
 
+! command -v atuin &>/dev/null && return
+
 [[ -f ~/.bash-preexec.sh ]] && source ~/.bash-preexec.sh
 
 eval "$(atuin init bash --disable-up-arrow --disable-ctrl-r)"
 
 # https://docs.atuin.sh/configuration/key-binding/#bash
 bind -x '"\C-]": "__atuin_history"'
+
+eval "$(atuin gen-completions --shell=bash)"
