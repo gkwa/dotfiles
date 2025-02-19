c6879180e2c7f53737a9c93602a92a8cf6b3f2a3
Taylor Monacelli <taylormonacelli@gmail.com>
2025-02-19 14:29:50 -0800

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,12 @@
+vim.opt.autoindent = true
 vim.opt.expandtab = false
 vim.opt.hlsearch = false
-vim.opt.autoindent = true
 vim.opt.ignorecase = true
 vim.opt.incsearch = true
 vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.showmatch = true
 vim.opt.smartcase = true
 vim.opt.tabstop = 4
-vim.opt.wrap = false
 vim.opt.undofile = true
+vim.opt.wrap = false
